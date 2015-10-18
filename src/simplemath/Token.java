4cbedc233b522fcfef270d838409d37b35e589f2
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2015-10-18 14:18:43 +0200

    add simplemath package

---
+++ src/simplemath/Token.java
@@ -0,0 +1,115 @@
+package simplemath;
+
+/**
+ * This class represents a single token of an arithmetic expression.
+ *
+ * @author 5hir0kur0
+ */
+class Token {
+
+    private final TokenType type;
+
+    /**
+     * Construct a {@code Token}.
+     * This constructor is only needed by the extending classes and to construct bracket tokens which are neither
+     * operators nor numbers.
+     * @param type
+     *     The {@code Token}'s {@code TokenType}
+     */
+    protected Token(TokenType type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Token["+type+"]";
+    }
+
+    /** @return the {@code char} representing the operator (if the {@code Token} is an {@code OperatorToken})*/
+    public char getOperator() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("getOperator() was called on Token.");
+    }
+
+    /** @return the number ({@code double}) held by the {@code Token} (if the {@code Token} is a {@code NumberToken} )*/
+    public double getNumber() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("getNumer() was called on Token.");
+    }
+
+    public TokenType getType() { return this.type; }
+
+    public boolean isOpeningBracket() { return this.type == TokenType.OPENING_BRACKET; }
+
+    public boolean isClosingBracket() { return this.type == TokenType.CLOSING_BRACKET; }
+
+    public boolean isNumber() { return this.type == TokenType.NUMBER; }
+
+    public boolean isOperator() { return this.type == TokenType.OPERATOR; }
+}
+
+/**
+ * This class represents a number (stored as double) in an arithmetic expression.
+ *
+ * @author 5hir0kur0
+ */
+class NumberToken extends Token {
+
+    private final double number;
+
+    NumberToken(double number) {
+        super(TokenType.NUMBER);
+        this.number = number;
+    }
+
+    /** If the {@code Token} is a {@code NumberToken} return the number ({@code double}) it holds.*/
+    @Override
+    public double getNumber() { return this.number; }
+
+    /** If the {@code Token} is an {@code OperatorToken}, return the {@code char} representing the operator.*/
+    @Override
+    public char getOperator() {
+        throw new UnsupportedOperationException("getOperator() was called on NumberToken.");
+    }
+
+    @Override
+    public String toString() {
+        return "NumberToken["+this.number+"]";
+    }
+}
+
+/**
+ * This class represents an operator in an arithmetic expression.
+ *
+ * @author 5hir0kur0
+ */
+class OperatorToken extends Token {
+
+    private final char operator;
+
+    /**
+     * @param operator
+     *     Only the operators described in Misc.VALID_OPERATORS can be stored using this class.
+     */
+    OperatorToken(char operator) {
+        super(TokenType.OPERATOR);
+        this.operator = operator;
+        if (!Misc.isValidOperator(operator))
+            throw new IllegalArgumentException("Invalid operator: "+operator);
+    }
+
+    /** If the {@code Token} is an {@code OperatorToken}, return the {@code char} representing the operator.*/
+    @Override
+    public char getOperator() throws UnsupportedOperationException {
+        return this.operator;
+    }
+
+    /** If the {@code Token} is a {@code NumberToken} return the number ({@code double}) it holds.*/
+    @Override
+    public double getNumber() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("getNumber() was called on OperatorToken.");
+    }
+
+    @Override
+    public String toString() {
+        return "OperatorToken["+this.operator+"]";
+    }
+}
---
+++ src/simplemath/TokenType.java
@@ -0,0 +1,10 @@
+package simplemath;
+
+/**
+ * Enumeration of the different types a {@code Token} of an arithmetic expression can have.
+ *
+ * @author 5hir0kur0
+ */
+public enum TokenType {
+    NUMBER, OPENING_BRACKET, CLOSING_BRACKET, OPERATOR;
+}
