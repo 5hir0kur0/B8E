f7873a7bd59774b9ef0549491c7f3177a6bbd6e5
5hir0kur0 <gordi.nerd@gmail.com>
2015-11-01 17:06:13 +0100

    add NumeralSystem and Register-interface
     * add enum NumeralSystem
     * add interface Register

---
+++ src/emulator/NumeralSystem.java
@@ -0,0 +1,72 @@
+package emulator;
+
+import org.omg.PortableInterceptor.LOCATION_FORWARD;
+
+/**
+ * This class represents a numeral system (binary, decimal or hexadecimal) and provides means of converting between
+ * different numeral systems.<br>
+ *
+ * Signed numbers are not supported.<br>
+ *
+ * The class uses the standard library to perform all the conversions.
+ *
+ * @author Gordian
+ */
+public enum NumeralSystem {
+    BINARY() {
+        @Override
+        public long getValue(String value) {
+            if (value.charAt(0) == '+') throw new NumberFormatException("No signs allowed in numbers.");
+            return Long.parseUnsignedLong(value, 2);
+        }
+    }, DECIMAL() {
+        @Override
+        public long getValue(String value) {
+            if (value.charAt(0) == '+') throw new NumberFormatException("No signs allowed in numbers.");
+            return Long.parseUnsignedLong(value, 10);
+        }
+    }, HEXADECIMAL() {
+        @Override
+        public long getValue(String value) {
+            if (value.charAt(0) == '+') throw new NumberFormatException("No signs allowed in numbers.");
+            return Long.parseUnsignedLong(value, 16);
+        }
+    };
+
+    /**
+     * Create a string representing the number stored in {@code value} in the numeral system specified by {@code target}
+     * @param target The {@code NumeralSystem} the value will be converted into.
+     * @param value The value to be converted into a {@code String}.
+     * @return {@code String} representing the number stored in {@code value} in the numeral system specified
+     *     by {@code target}
+     */
+    public static String toString(NumeralSystem target, long value) {
+        switch (target) {
+            case BINARY: return Long.toUnsignedString(value, 2);
+            case DECIMAL: return Long.toUnsignedString(value, 10);
+            case HEXADECIMAL: return Long.toUnsignedString(value, 16);
+            default: throw new UnsupportedOperationException("Illegal numeral system: "+target);
+        }
+    }
+
+    /**
+     * Convert a string representing a number in the numeral system represented by the current instance to a long
+     * using {@code Long.parseUnsignedLong(...)}
+     * @param value string representing a number in this instance's numeral system
+     * @throws NumberFormatException whenever {@code Long.parseUnsignedLong(...)} throws it and if the expression
+     *     starts with a '+'.
+     * @return the result of the conversion
+     */
+    public abstract long getValue(String value) throws NumberFormatException;
+
+    /**
+     * Create a string representing the number stored in {@code value} in the numeral system represented by
+     * this instance.
+     * @param value The value to be converted into a {@code String}.
+     * @return {@code String} representing the number stored in {@code value} in the numeral system represented by
+     *     this instance.
+     */
+    public String toString(long value) {
+        return NumeralSystem.toString(this, value);
+    }
+}
---
+++ src/emulator/Register.java
@@ -0,0 +1,91 @@
+package emulator;
+
+/**
+ * This interface represents a CPU register. It has setters and getters for binary, hex and decimal strings that
+ * represent a register value.<br>
+ * A register has a name (e.g. "A") and a numerical value (e.g. 42).
+ *
+ * @author Gordian
+ */
+public interface Register {
+    /**
+     * @return the {@code Register}'s name (cannot be {@code null})
+     */
+    String getName();
+
+    /**
+     * @param target {@code NumeralSystem} used to perform the conversion
+     * @return {@code String} representing the {@code Register}'s (unsigned) value in the numeral system specified by
+     *     {@code target}
+     */
+    String getDisplayValue(NumeralSystem target);
+
+    /**
+     * @return {@code String} representing the {@code Register}'s (unsigned) value in binary.
+     */
+    default String getBinaryDisplayValue() {
+        return getDisplayValue(NumeralSystem.BINARY);
+    }
+
+    /**
+     * @return {@code String} representing the {@code Register}'s (unsigned) value in decimal.
+     */
+    default String getDecimalDisplayValue() {
+        return getDisplayValue(NumeralSystem.DECIMAL);
+    }
+
+    /**
+     * @return {@code String} representing the {@code Register}'s (unsigned) value in hexadecimal.
+     */
+    default String getHexadecimalDisplayValue() {
+        return getDisplayValue(NumeralSystem.HEXADECIMAL);
+    }
+
+    /**
+     * Set the {@code Register}'s value to the value represented by {@code newValue} in the specified numeral system.
+     * <br>
+     * This operation is expected to fail quite often (as it relies on user input), hence it returns a {@code boolean}
+     * instead of throwing an exception.
+     * @param numeralSystem {@code NumeralSystem} used for the conversion
+     * @param newValue {@code String} that holds the value. Must not be {@code null} or empty.
+     * @return {@code boolean} indicating whether the operation was successful.
+     */
+    boolean setValueFromString(NumeralSystem numeralSystem, String newValue);
+
+    /**
+     * Set the {@code Register}'s value to the value represented by {@code newValue} in binary.
+     * <br>
+     * This operation is expected to fail quite often (as it relies on user input), hence it returns a {@code boolean}
+     * instead of throwing an exception.
+     * @param newValue {@code String} that holds the value. Must not be {@code null} or empty.
+     * @return {@code boolean} indicating whether the operation was successful.
+     */
+    default boolean setValueFromBinaryString(String newValue) {
+        return setValueFromString(NumeralSystem.BINARY, newValue);
+    }
+
+    /**
+     * Set the {@code Register}'s value to the value represented by {@code newValue} in decimal.
+     * <br>
+     * This operation is expected to fail quite often (as it relies on user input), hence it returns a {@code boolean}
+     * instead of throwing an exception.
+     * @param newValue {@code String} that holds the value. Must not be {@code null} or empty.
+     * @return {@code boolean} indicating whether the operation was successful.
+     */
+    default boolean setValueFromDecimalString(String newValue) {
+        return setValueFromString(NumeralSystem.DECIMAL, newValue);
+    }
+
+
+    /**
+     * Set the {@code Register}'s value to the value represented by {@code newValue} in hexadecimal.
+     * <br>
+     * This operation is expected to fail quite often (as it relies on user input), hence it returns a {@code boolean}
+     * instead of throwing an exception.
+     * @param newValue {@code String} that holds the value. Must not be {@code null} or empty.
+     * @return {@code boolean} indicating whether the operation was successful.
+     */
+    default boolean setValueFromHexadecimalString(String newValue) {
+        return setValueFromString(NumeralSystem.HEXADECIMAL, newValue);
+    }
+}
