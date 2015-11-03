e3074154db5167b52ff020e3de49477644a0e307
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2015-11-03 16:42:20 +0100

    add ROM interface and RAM class
     * change BitAddressableByteRegister and ByteRegister to include
       a constructor that accepts only a name and sets the initial
       value to 0

---
+++ src/emulator/RAM.java
@@ -0,0 +1,109 @@
+package emulator;
+
+/**
+ * This class represents Random Access Memory (RAM)
+ *
+ * @author 5hir0kur0
+ */
+public class RAM implements ROM {
+    protected byte[] memory;
+    protected int minIndex;
+    protected int maxIndex;
+
+    /**
+     * Create a new RAM object.
+     *
+     * @param size The number of bytes in the created object. Must be bigger than 0.
+     * @param minAddress The smallest address that can be accessed. Must be bigger than 0 and smaller than size and
+     *                   smaller than maxAddress.
+     * @param maxAddress The biggest address that can be accessed. Must be bigger than 0 and bigger than minAddress and
+     *                   smaller than size.
+     */
+    RAM(int size, int minAddress, int maxAddress) {
+        if (size <= 0)
+            throw new IllegalArgumentException("Cannot create ROM of size smaller than or equal to 0");
+        if (minAddress < 0 || minAddress >= size)
+            throw new IllegalArgumentException("The minimal address is out of range");
+        if (maxAddress >= size || maxAddress < 0)
+            throw new IllegalArgumentException("The maximal address is out of range");
+        if (minAddress >= maxAddress)
+            throw new IllegalArgumentException("The minimal address must not be smaller than or equal to the maximal"+
+                                               "address.");
+        this.memory = new byte[size];
+        this.minIndex = minAddress;
+        this.maxIndex = maxAddress;
+    }
+
+    /**
+     * Create a new RAM object.
+     *
+     * @param size The number of bytes in the created object. Must be bigger than 0.
+     */
+    RAM(int size) {
+        this(size, 0, size - 1);
+    }
+
+    @Override
+    public byte get(int index) {
+        return this.memory[index];
+    }
+
+    /**
+     * Get a number of {@code Byte}s from memory.
+     * Whenever a value does not exist in memory, {@code  null} is written to
+     * the respective index in the returned array.
+     * @param index The start index.
+     * @param length The number of {@code Byte}s to be returned.
+     * @return an array of {@code Byte}s (if a value in the array is {@code null} this means that it does not exist in
+     *         memory.
+     */
+    @Override
+    public Byte[] get(int index, int length) {
+        if (length <= 0)
+            throw new IllegalArgumentException("length cannot be smaller than or equal to 0");
+        Byte[] ret = new Byte[length];
+        for (int i = 0; i < ret.length; i++) {
+            if (i < minIndex || i > maxIndex) {
+                ret[i] = null;
+                continue;
+            }
+            ret[i] = this.memory[i];
+        }
+        return ret;
+    }
+
+    @Override
+    public int getMinAddress() {
+        return this.minIndex;
+    }
+
+    @Override
+    public int getMaxAddress() {
+        return this.maxIndex;
+    }
+
+    @Override
+    public void setMinAddress(int address) {
+        if (address < 0 || address >= this.maxIndex || address >= this.memory.length)
+            throw new IllegalArgumentException("address out of range");
+        this.minIndex = address;
+    }
+
+    @Override
+    public void setMaxAddress(int address) {
+        if (address >= this.memory.length || address <= this.minIndex || address < 0)
+            throw new IllegalArgumentException("address out of range");
+        this.maxIndex = address;
+    }
+
+    public void set(int index, byte value) {
+        if (index < this.minIndex || index > this.maxIndex)
+            throw new IllegalArgumentException("index out of range");
+        this.memory[index] = value;
+    }
+
+    @Override
+    public int getSize() {
+        return this.memory.length;
+    }
+}
---
+++ src/emulator/ROM.java
@@ -0,0 +1,40 @@
+package emulator;
+
+/**
+ * This interface represents Read Only Memory (ROM).
+ *
+ * @author 5hir0kur0
+ */
+public interface ROM {
+
+    byte get(int index);
+
+    /**
+     * Get a number of {@code Byte}s from memory.
+     * Whenever a value does not exist in memory, {@code  null} is written to
+     * the respective index in the returned array.
+     * @param index The start index.
+     * @param length The number of {@code Byte}s to be returned.
+     * @return an array of {@code Byte}s (if a value in the array is {@code null} this means that it does not exist in
+     *         memory.
+     */
+    Byte[] get(int index, int length);
+
+    int getMinAddress();
+
+    int getMaxAddress();
+
+    int getSize();
+
+    /**
+     * @param address The smallest address that can be accessed. Must be bigger than 0 and smaller than size and
+     *                smaller than maxAddress.
+     */
+    void setMinAddress(int address);
+
+    /**
+     * @param address The biggest address that can be accessed. Must be bigger than 0 and bigger than minAddress and
+     *                smaller than size.
+     */
+    void setMaxAddress(int address);
+}
