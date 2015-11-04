92005125b733ddb516da1d1221af7955117fd188
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2015-11-04 22:58:55 +0100

    Add more 'API' classes
    Add Mnemonic.java and supporting classes
    + Add class that represents a mnemonic of a assembly language.
      Has a method to output assembled code
    + Add Operand.java that represents a mnemonic operand.
      It has a type and a value
    
    Add Preprocessor.java
    + Use to preprocess assembler code
    + Will support defines and '\' line merges
    
    Start to add 8051 classes
    + Add Mnemonic8051.java that is a specification of Mnemonic.
      Uses a short for the code point
    + Add MC8051Libary.java that will contain the mnemonics of the
      8051 assembly language
    + Add an nested enum in MC8051Libary that specifies the operand
      types for the 8051 assembly language
    
    * All classes are properly unfinished.

---
+++ src/assembler/Operand.java
@@ -0,0 +1,84 @@
+package assembler;
+
+/**
+ * Represents an operand of a mnemonic.
+ *
+ * @author Noxgrim
+ */
+public class Operand {
+
+    /** Type of the operand. Should be named via <code>final</code> variables. */
+    private int type;
+
+    /** The value of the operand as a String. */
+    private String value;
+
+    /**
+     * Constructs a new Operand.
+     *
+     * @param type
+     *      the type of the Operand.<br>
+     *      To remember the type better its value should be
+     *      saved in a named final variable or an enum's ordinal
+     *      and it should be used instead.<br>
+     * @param value
+     *      the value of the Operand as a String.
+     */
+    public Operand(int type, String value) {
+        this.type = type;
+
+        if (value == null || value.isEmpty())
+            throw new IllegalArgumentException("Value cannot be 'null' or empty.");
+        this.value = value;
+    }
+
+    /**
+     * @return
+     *      the type of the operand as an integer.
+     */
+    public int getType() {
+        return type;
+    }
+
+    /**
+     * @return
+     *      whether the given type equals the type of the operand.
+     */
+    public boolean isType(int type) {
+        return this.type == type;
+    }
+
+    /**
+     * @return
+     *      the value of this operand as a String.
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * @param value
+     *      the value of this operand as a String.
+     * @return
+     *      the Operand itself.
+     */
+    public Operand setValue(String value) {
+        if (!(value == null || value.isEmpty()))
+            this.value = value;
+        return this;
+    }
+
+    /**
+     * @param type
+     *      the type to be set.<br>
+     *      To remember the type better its value should be
+     *      saved in a named final variable or an enum's ordinal
+     *      and it should be used instead.<br>
+     * @return
+     *      the Operand itself.
+     */
+    public Operand setType(int type) {
+        this.type = type;
+        return this;
+    }
+}
---
+++ src/assembler/arc8051/Mnemonic8051.java
@@ -0,0 +1,23 @@
+package assembler.arc8051;
+
+import assembler.Mnemonic;
+
+/**
+ * A mnemonic for a microcomputer with a
+ * 8051-family architecture.
+ *
+ * @author Noxgrim
+ */
+public abstract class Mnemonic8051 extends Mnemonic<Short> {
+    /**
+     * Constructs a new 8051 Mnemonic.
+     *
+     * @param name
+     *      The name of the mnemonic. It is used to differentiate
+     *      this mnemonic from other ones with other functions.<br>
+     *      The name will be converted to lower case.
+     */
+    protected Mnemonic8051(String name) {
+        super(name);
+    }
+}
