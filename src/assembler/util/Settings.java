102c08a433d25fb9b3efbc32bcd317562812179f
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2015-12-12 19:09:06 +0100

    Add "add"-mnemonic and fix stuff
    New mnemonic
    + Add "add"-mnemonic to MC8051Libary
    + Add "Ignore obvious operands" setting to Settings
    * Move ErrorHandling to Errors
    
    Fixes
    * Add validation code to AssembyError's constructor
    * Change "acall"-mnemonic to use an empty array as default
    
    + Add JavaDocs to settings
    
    Todo: Add some kind of multi error handler to mnemonic

--- src/assembler/util/Settings.java
+++ src/assembler/util/Settings.java
@@ -6,12 +6,31 @@ package assembler.util;
  */
 public class Settings {
 
-    public enum  ErrorHandling {
-        IGNORE, WARN, ERROR;
-    }
-
     public static class Errors {
-        public static ErrorHandling ADDITIONAL_OPERANDS = ErrorHandling.WARN;
+        /**
+         * How additional operands like
+         * <pre>
+         *     NOP <i><b>#42 #21</b></i>
+         * </pre>
+         * should be handled.
+         */
+        public static ErrorHandling ADDITIONAL_OPERANDS = ErrorHandling.ERROR;
+        /**
+         * How the ignoring of obvious operands like
+         * <pre>
+         *     ADD <i><b>a</b></i>, #42
+         *     ==
+         *     ADD #42
+         * </pre>
+         * should be handled.<br>
+         * The <code>a</code> operand is obvious in this case because
+         * all variants of <code>ADD</code> use it at this position.
+         */
+        public static ErrorHandling IGNORE_OBVIOUS_OPERANDS = ErrorHandling.ERROR;
+
+        public enum  ErrorHandling {
+            IGNORE, WARN, ERROR;
+        }
     }
 
 }
