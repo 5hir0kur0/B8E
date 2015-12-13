4129801f22772142f3e47226c67abfa3cfb55bbe
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2015-12-13 15:53:52 +0100

    Add new mnemonics and updated mnemonic system
    Mnemonics
    + Add "addc", "ajump" (as almost identical copies of "add" and "acall")
    + Add "anl"
    * Change size error message in "add/addc" to be the same as in "anl"
    
    Mnemonic system
    + Add 'min operands' attribute
    + Add 'position sentive' attribute to mark mnemonics that change with their position in code memory
    + Add corresponding getter
    * Add new fields to constructor parameters
    + Add constructor without 'position sentive' parameter
    
    Other
    * Add JavaDoc to 'getInstructionFromOperands' in Mnemonic 8051
    * Add check for empty 'name' to Mnemonic's constructor
    * Add bit lengths to the JavaDocs of OperandType8051
    
    Fixes
    * Fix missing "L" notation in bit operantions
    * Fix wrong error statement if IGNORE_OBVIOUS_OPERANDS was enabled
      ("add","addc")
    * Fix missing check for only one operand if the first wasn't 'a' in "add"
    * Fix wrong check for "r" in "add"
    * Fix wrong opcode in "acall"
    * Fix missing 'null' check in Mnemonic
    * Fix some issues in the OperandType8051 JavaDocs

--- src/assembler/arc8051/MC8051Libary.java
+++ src/assembler/arc8051/MC8051Libary.java
@@ -22,7 +22,8 @@ public class MC8051Libary {
          * language all constant values have
          * a number sign '#' as prefix.<br>
          * All numbers must be present in decimal
-         * system (10) form.
+         * system (10) form and have to be <code>8</code>
+         * or <code>16</code> bit long.
          * The value has to be the raw value
          * without the '#'.
          */
@@ -31,13 +32,17 @@ public class MC8051Libary {
          * An address.<br> In 8051 assembly
          * language addresses have no prefix.<br>
          * All addresses must be present in decimal
-         * system (10) form.
+         * system (10) form and have to be <code>8</code>
+         * or <code>16</code> bit long.
          */
         ADDRESS,
         /**
-         * An 'negated' address. In 8051 assembly
+         * A 'negated' address.<br> In 8051 assembly
          * language all negated addresses have a
          * solidus '/' as prefix.<br>
+         * All addresses must be present in decimal
+         * system (10) form and have to be <code>8</code>
+         * bit long.<br>
          * Negated addresses are used in bit operations
          * to indicate that the  microcomputer first
          * has to negate the value at the value at the
@@ -69,7 +74,7 @@ public class MC8051Libary {
 
     public static final Mnemonic8051[] mnemonics = {
 
-            new Mnemonic8051("acall") {
+            new Mnemonic8051("acall", 1, true) {
                 @Override
                 public byte[] getInstructionFromOperands(long codePoint, OperandToken8051... operands) {
                     byte[] result = new byte[0];
@@ -77,7 +82,7 @@ public class MC8051Libary {
                         long jump = Long.parseLong(operands[0].getValue()) + 2;
 
                         if ((jump >>> 0xbL & 0x1fL) == (codePoint >>> 0xbL & 0x1fL))
-                            result = new byte[]{((byte)(jump >>>0x5L & 0xf00L | 0x11)), (byte)(jump & 0xff)};
+                            result = new byte[]{((byte)(jump >>>0x5L & 0xf00L | 0x11L)), (byte)(jump & 0xffL)};
                         else
                             operands[0].setError(new SimpleAssemblyError(Type.ERROR,
                                     "Call address too far far absolute 11 bit addressing!"));
@@ -92,7 +97,63 @@ public class MC8051Libary {
                 }
             },
 
-            new Mnemonic8051("add") {
+            new Mnemonic8051("add", 1) {
+                @Override
+                public byte[] getInstructionFromOperands(long codePoint, OperandToken8051... operands) {
+                    byte[] result = new byte[0];
+
+                    boolean firstIsA = true;
+
+                    if (!(operands[0].getOperandType() == OperandType8051.NAME &&
+                            operands[0].getValue().equals("a"))) {
+                        firstIsA = false;
+                        operands[0].setError(getErrorFromErrorHandlingSetting(Settings.Errors.IGNORE_OBVIOUS_OPERANDS,
+                                "Missing 'a' as first operand!", "Operand 'a' should be written as first operand."));
+                    } else if (operands.length < 2) {
+                        operands[0].setError(new SimpleAssemblyError(Type.ERROR, "Expected 2 operands!"));
+                        return new byte[0];
+                    }
+
+                    final OperandToken8051 op = operands[firstIsA?1:0];
+                    final OperandType8051 type = op.getOperandType();
+                    switch (op.getOperandType()) {
+                        case CONSTANT:
+                        case ADDRESS: {
+                            int value = Integer.parseInt(op.getValue());
+                            if (value > 0xFF)
+                                op.setError(new SimpleAssemblyError(Type.ERROR, "Value of " +
+                                        (type == OperandType8051.ADDRESS ? "direct address" : "constant" ) + " too big!"));
+                            else
+                                result = new byte[]{(byte) (type == OperandType8051.ADDRESS ? 0x25:0x24), (byte) value};
+                            break;
+                        }
+                        case INDIRECT_NAME:
+                        case NAME:
+                            if (op.getValue().startsWith("r")) {
+                                int ordinal = Integer.parseInt(op.getValue().substring(1));
+                                if (ordinal > (type == OperandType8051.NAME? 7 : 1))
+                                    op.setError(new SimpleAssemblyError(Type.ERROR, "Register ordinal too high!"));
+                                else {
+                                    result = new  byte[] {(byte)(ordinal|(type==OperandType8051.NAME?0x28:0x26))};
+                                    break;
+                                }
+                            }
+                        default:
+                            op.setError(new SimpleAssemblyError(Type.ERROR, "Incompatible operand!"));
+                    } //TODO Multiple error handling.
+
+                    final String err = Settings.Errors.IGNORE_OBVIOUS_OPERANDS == ErrorHandling.ERROR ?
+                            "Too many operands! ADD must have exactly 2 operands." :
+                            "Too many operands! ADD must have 1 or 2 operands.";
+                    for (int i = firstIsA?2:1; i < operands.length; ++i)
+                        operands[i].setError(getErrorFromErrorHandlingSetting(Settings.Errors.ADDITIONAL_OPERANDS,
+                                err, "Unnecessary operand."));
+
+                    return result;
+                }
+            },
+
+            new Mnemonic8051("addc", 1) {
                 @Override
                 public byte[] getInstructionFromOperands(long codePoint, OperandToken8051... operands) {
                     byte[] result = new byte[0];
@@ -104,6 +165,9 @@ public class MC8051Libary {
                         firstIsA = false;
                         operands[0].setError(getErrorFromErrorHandlingSetting(Settings.Errors.IGNORE_OBVIOUS_OPERANDS,
                                 "Missing 'a' as first operand!", "Operand 'a' should be written as first operand."));
+                    } else if (operands.length < 2) {
+                        operands[0].setError(new SimpleAssemblyError(Type.ERROR, "Expected 2 operands!"));
+                        return new byte[0];
                     }
 
                     final OperandToken8051 op = operands[firstIsA?1:0];
@@ -113,20 +177,20 @@ public class MC8051Libary {
                         case ADDRESS: {
                             int value = Integer.parseInt(op.getValue());
                             if (value > 0xFF)
-                                op.setError(new SimpleAssemblyError(Type.ERROR, (type == OperandType8051.ADDRESS ?
-                                        "Direct address" : "Constant" ) + "value is too big!"));
+                                op.setError(new SimpleAssemblyError(Type.ERROR, "Value of " +
+                                        (type == OperandType8051.ADDRESS ? "direct address" : "constant" ) + " too big!"));
                             else
-                                result = new byte[]{(byte) (type == OperandType8051.ADDRESS ? 0x25:0x24), (byte) value};
+                                result = new byte[]{(byte) (type == OperandType8051.ADDRESS ? 0x35:0x34), (byte) value};
                             break;
                         }
                         case INDIRECT_NAME:
                         case NAME:
-                            if (op.getValue().charAt(0) != 'r') {
+                            if (op.getValue().startsWith("r")) {
                                 int ordinal = Integer.parseInt(op.getValue().substring(1));
                                 if (ordinal > (type == OperandType8051.NAME? 7 : 1))
                                     op.setError(new SimpleAssemblyError(Type.ERROR, "Register ordinal too high!"));
                                 else {
-                                    result = new  byte[] {(byte)(ordinal|(type==OperandType8051.NAME?0x48:0x46))};
+                                    result = new  byte[] {(byte)(ordinal|(type==OperandType8051.NAME?0x38:0x36))};
                                     break;
                                 }
                             }
@@ -134,9 +198,138 @@ public class MC8051Libary {
                             op.setError(new SimpleAssemblyError(Type.ERROR, "Incompatible operand!"));
                     } //TODO Multiple error handling.
 
+                    final String err = Settings.Errors.IGNORE_OBVIOUS_OPERANDS == ErrorHandling.ERROR ?
+                            "Too many operands! ADD must have exactly 2 operands." :
+                            "Too many operands! ADD must have 1 or 2 operands.";
+                    for (int i = firstIsA?2:1; i < operands.length; ++i)
+                        operands[i].setError(getErrorFromErrorHandlingSetting(Settings.Errors.ADDITIONAL_OPERANDS,
+                                err, "Unnecessary operand."));
+
+                    return result;
+                }
+            },
+
+            new Mnemonic8051("ajmp", 1, true) {
+                @Override
+                public byte[] getInstructionFromOperands(long codePoint, OperandToken8051... operands) {
+                    byte[] result = new byte[0];
+                    if (operands[0].getOperandType() == OperandType8051.ADDRESS) {
+                        long jump = Long.parseLong(operands[0].getValue()) + 2;
+
+                        if ((jump >>> 0xbL & 0x1fL) == (codePoint >>> 0xbL & 0x1fL))
+                            result = new byte[]{((byte)(jump >>>0x5L & 0xf00L | 0x1L)), (byte)(jump & 0xffL)};
+                        else
+                            operands[0].setError(new SimpleAssemblyError(Type.ERROR,
+                                    "Jump address too far far absolute 11 bit addressing!"));
+                    } else
+                        operands[0].setError(new SimpleAssemblyError(Type.ERROR,
+                                "Operand needs to be an address!"));
+
                     for (int i = 1; i < operands.length; ++i)
                         operands[i].setError(getErrorFromErrorHandlingSetting(Settings.Errors.ADDITIONAL_OPERANDS,
-                                "Too many operands! ADD must have exactly 2 operands.", "Unnecessary operand."));
+                                "Too many operands! AJMP must have exactly 1 operand.", "Unnecessary operand."));
+                    return result;
+                }
+            },
+
+            new Mnemonic8051("anl", 1) {
+                @Override
+                public byte[] getInstructionFromOperands(long codePoint, OperandToken8051... operands) {
+                    byte[] result = new  byte[0];
+                    OperandToken8051 op1 = operands[0];
+                    boolean firstIgnored = false;
+
+                    switch (op1.getOperandType()) {
+                        case NEGATED_ADDRESS: {
+                            firstIgnored = true;
+                            op1.setError(getErrorFromErrorHandlingSetting(Settings.Errors.IGNORE_OBVIOUS_OPERANDS,
+                                    "Missing 'c' as first operand!", "Operand 'c' should be written as first operand."));
+                            result = new byte[]{(byte)0xb0};
+                        }
+                        case INDIRECT_NAME:
+                        case NAME:
+                            if (op1.getValue().startsWith("r")) {
+                                OperandType8051 type = op1.getOperandType();
+                                firstIgnored = true;
+                                op1.setError(getErrorFromErrorHandlingSetting(Settings.Errors.IGNORE_OBVIOUS_OPERANDS,
+                                        "Missing 'a' as first operand!", "Operand 'a' should be written as first operand."));
+
+                                int ordinal = Integer.parseInt(op1.getValue().substring(1));
+                                if (ordinal > (type == OperandType8051.NAME? 7 : 1))
+                                    op1.setError(new SimpleAssemblyError(Type.ERROR, "Register ordinal too high!"));
+                                else {
+                                    result = new  byte[] {(byte)(ordinal|(type==OperandType8051.NAME?0x58:0x56))};
+                                    break;
+                                }
+                        }
+
+                        if (!firstIgnored) {
+                            if (operands.length < 2) {
+                                op1.setError(new SimpleAssemblyError(Type.ERROR, "Expected 2 operands!"));
+                                return new byte[0];
+                            }
+                            OperandToken8051 op2 = operands[1];
+
+                            switch (op1.getOperandType()) {
+                                case ADDRESS: {
+                                    int value = Integer.parseInt(op1.getValue());
+                                    if (value <= 0xff)
+                                        switch (op2.getOperandType()) {
+                                            case CONSTANT: {
+                                                int constVal = Integer.parseInt(op2.getValue());
+                                                if (constVal <= 0xff) {
+                                                    result = new byte[]{0x53, (byte) value, (byte) constVal};
+                                                    break;
+                                                } else
+                                                    op2.setError(new SimpleAssemblyError(Type.ERROR,
+                                                            "Value of constant too big!"));
+                                                break;
+                                            }
+                                            case NAME:
+                                                if (op2.getValue().equals("a")) {
+                                                    result = new byte[]{(byte) 0x52, (byte) value};
+                                                    break;
+                                                }
+                                            default:
+                                                op2.setError(new SimpleAssemblyError(Type.ERROR, "Incompatible operand!"));
+                                        }
+                                    else
+                                        op1.setError(new SimpleAssemblyError(Type.ERROR, "Value of direct address too big!"));
+                                    break;
+                                }
+                                case NAME:
+                                    if (op1.getValue().startsWith("a")) {
+                                        if (op2.getOperandType() == OperandType8051.CONSTANT
+                                                || op2.getOperandType() == OperandType8051.ADDRESS) {
+                                            int value = Integer.parseInt(op2.getValue());
+                                            OperandType8051 type = op2.getOperandType();
+                                            if (value <= 0xff)
+                                                result = new byte[]{(byte) (type == OperandType8051.CONSTANT ? 0x54 : 0x55),
+                                                        (byte) value};
+                                            else
+                                                op2.setError(new SimpleAssemblyError(Type.ERROR, "Value of " +
+                                                        (type == OperandType8051.CONSTANT ? "constant" : "direct address") +
+                                                        " too big!"));
+                                        } else
+                                            op2.setError(new SimpleAssemblyError(Type.ERROR, "Incompatible operand!"));
+                                        break;
+                                    }
+                                default:
+                                    op1.setError(new SimpleAssemblyError(Type.ERROR, "Incompatible operand!"));
+
+                            }
+                        }
+
+                    }
+
+
+                    final String err = Settings.Errors.IGNORE_OBVIOUS_OPERANDS == ErrorHandling.ERROR ?
+                            "Too many operands! ANL must have exactly 2 operands." :
+                            "Too many operands! ANL must have 1 or 2 operands.";
+                    for (int i = firstIgnored?2:1; i < operands.length; ++i)
+                        operands[i].setError(getErrorFromErrorHandlingSetting(Settings.Errors.ADDITIONAL_OPERANDS,
+                                err, "Unnecessary operand."));
+
                     return result;
                 }
             }
