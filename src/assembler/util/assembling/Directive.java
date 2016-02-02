e46a964aef8c784b94e4b1e86d1b3f1c50c23e4a
Noxgrim <2074494+Noxgrim@users.noreply.github.com>
2016-02-02 13:01:48 +0100

    Add Directive and Settings
    Settings
    + Add following settings:
      * Intel-Hex File extension
      * END-missing
      * Enforce END behavior
    * Change Assembler to support new Settings
    - Remove old extension Setting
    * Make most settings Strings public
    
    Directives
    + Add Directive class

---
+++ src/assembler/util/assembling/Directive.java
@@ -0,0 +1,44 @@
+package assembler.util.assembling;
+
+import java.util.Objects;
+
+/**
+ * Represents a Assembler directive.
+ *
+ * @author Noxgrim
+ */
+public abstract class Directive {
+
+    /** The name of the directive. */
+    private String name;
+
+    /**
+     * Constructs a new Directive.
+     *
+     * @param name
+     *      the name of the Directive.
+     */
+    public Directive(String name) {
+        if ((this.name = Objects.requireNonNull(name, "Name cannot be 'null'.")).trim().isEmpty())
+            throw new IllegalArgumentException("Name cannot be empty or white space only.");
+    }
+
+    /**
+     * Returns the name of the directive.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Performs the directive.
+     *
+     * @param args
+     *      the arguments of the directive.
+     *
+     * @return
+     *      whether the directive was performed
+     *      successfully.
+     */
+    public abstract boolean perform(String ... args);
+}
