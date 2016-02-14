98a50e1bade3d1093faff52391cbf36d8e4df6bf
Jannik <polymehr.service@gmail.com>
2016-02-14 01:09:04 +0100

    Add Regex implementation
    Regex
    + Add Regex class
    + Add Settings
      + "assembler.errors.regex.same-match-case"
      + "assembler.errors.regex.unnecessary-segments"
      + "assembler.directives.regex.default-flags"
    
    Settings
    + Add 'getBoolProperty()'
    
    Problems
    + Add 'problems' and 'line' field to Preprocessor8051
    + Add 'file' and 'line' to PreprocessorProblem
    + Add constructor without 'message' and 'type' to PreprocessorProblem
    + Add 'printStackTrace()' methods to ExceptionProblem
    + Add 'getStackTraceAsString()' to ExceptionProblem
    
    Other
    + Add 'addGeneralErrorSetting()' method to MC8051Library
    + Start to implement test for Regex
    * Change indent (and <br>)in AssemblerSettings JavaDocs

--- src/assembler/arc8051/Preprocessor8051.java
+++ src/assembler/arc8051/Preprocessor8051.java
@@ -1,12 +1,15 @@
 package assembler.arc8051;
 
 import assembler.Preprocessor;
+import assembler.util.Regex;
+import assembler.util.assembling.Directive;
 import assembler.util.problems.ExceptionProblem;
 import assembler.util.problems.PreprocessingProblem;
 import assembler.util.problems.Problem;
 
 import java.io.*;
-import java.util.ArrayList;
+import java.nio.file.Path;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
 
@@ -17,12 +20,35 @@ import java.util.regex.Matcher;
  * @author Jannik
  */
 public class Preprocessor8051 implements Preprocessor {
+
+    private List<Problem> problems;
+
+    private Path currentFile;
+    private int line;
+
+    private final Directive[] direktives = {
+            new Directive("equ") {
+                @Override
+                public boolean perform(String... args) {
+                    //Regex test = new Regex("");
+                    return false;
+                }
+            }
+    };
+
+    public Preprocessor8051() {
+        line = 0;
+        problems = new LinkedList<>();
+    }
+
     @Override
     public List<? extends Problem> preprocess(BufferedReader input, StringWriter output) {
+        problems.clear();
+        line = 0;
         String line, original;
-        List<Problem> problems = new ArrayList<>();
         try {
-            while ((line = original =input.readLine()) != null) {
+            while ((line = original = input.readLine()) != null) {
+                this.line++;
                 line = lowerCase(line, problems);
 
                 line = handleDirective(line, problems);
@@ -69,7 +95,7 @@ public class Preprocessor8051 implements Preprocessor {
      */
     private String lowerCase(String line, List<Problem> problems) {
 
-        StringBuffer result = new StringBuffer(line.length());
+        StringBuilder result = new StringBuilder(line.length());
         boolean simpQuoted = false, doubQuoted = false;
 
         int last = 0;
@@ -78,6 +104,7 @@ public class Preprocessor8051 implements Preprocessor {
                 break;
             else if (last == '\\' && !(cp == '\'' || cp == '"' || cp != '\\'))
                 problems.add(new PreprocessingProblem("Illegal escape!", Problem.Type.ERROR,
+                        currentFile, this.line,
                         new StringBuilder().appendCodePoint(last).appendCodePoint(cp).toString()));
             else if (last == '\\')
                 result.appendCodePoint(cp);
@@ -93,7 +120,9 @@ public class Preprocessor8051 implements Preprocessor {
             last = cp;
         }
         if (doubQuoted || simpQuoted)
-            problems.add(new PreprocessingProblem("Unclosed quote!", Problem.Type.WARNING, line));
+            problems.add(new PreprocessingProblem("Unclosed quote!",
+                    Problem.Type.WARNING, currentFile, this.line, line));
         return result.toString();
     }
+
 }
