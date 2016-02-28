d39ec1db090b8ca6108a70c7ddbb9fba595147f2
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2016-02-28 17:57:14 +0100

    Add syntax highlighting
    + Add regex for Java Properties files
    + Add unfinished regex for Intel HEX files

---
+++ src/misc/Pair.java
@@ -0,0 +1,14 @@
+package misc;
+
+/**
+ * Simple generic class to store a pair of items.
+ * @author 5hir0kur0
+ */
+public class Pair<X, Y> {
+    public final X x;
+    public final Y y;
+    public Pair(X x, Y y) {
+        this.x = x;
+        this.y = y;
+    }
+}
