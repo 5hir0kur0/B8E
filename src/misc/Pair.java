9b1f0ddfe0c442453da99dac3d23d5f5702c7c14
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2016-05-26 18:00:31 +0200

    Add (not working) JAXB serialization for themes

--- src/misc/Pair.java
+++ src/misc/Pair.java
@@ -1,9 +1,13 @@
 package misc;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
 /**
  * Simple generic class to store a pair of items.
  * @author 5hir0kur0
  */
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Pair<X, Y> {
     public X x;
     public Y y;
